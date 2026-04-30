efe0b61210a6457ec231a74344847ee57fe90f44
DamianMirror <dematchuma2017@gmail.com>
2026-04-30 19:13:14 +0300

    fix: changed vm size

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -19,7 +19,7 @@ variable "vm_name" {
 variable "vm_size" {
   description = "VM size"
   type        = string
-  default     = "Standard_B2s"
+  default     = "Standard_B1ms"
 }
 
 variable "admin_username" {
